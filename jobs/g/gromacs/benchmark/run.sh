9ff6b29af9ddd5206ddf277dccfba6c320d7a4fa
Qin Wan <rogerwq@gmai.com>
2025-11-12 09:23:31 +0000

    ready to merge

--- jobs/g/gromacs/benchmark/run.sh
+++ jobs/g/gromacs/benchmark/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
-URL="https://www.mpinat.mpg.de/3925290/hif2a_eq.zip"
-FILENAME="hif2a_eq.zip"
+SAMPLE="hif2a_eq"
+URL="https://www.mpinat.mpg.de/3925290/${SAMPLE}.zip"
+FILENAME="${SAMPLE}.zip"
 
 echo "Attempting to download $FILENAME from $URL..."
 # Download the zip file using wget
-# -q: quiet (suppress output)
-# -O: output document to filename
 if ! wget -q -O "$FILENAME" "$URL"; then
     echo "Error: Failed to download $FILENAME from $URL"
     exit 1
@@ -16,11 +15,12 @@ echo "$FILENAME downloaded successfully."
 
 echo "Attempting to unzip $FILENAME..."
 # Unzip the downloaded file
-# -o: overwrite files without prompting
-# -d: extract files into the specified directory (optional, current directory by default)
 if ! unzip -o "$FILENAME"; then
     echo "Error: Failed to unzip $FILENAME"
     exit 1
 fi
 echo "$FILENAME unzipped successfully."
 echo "You can now find the extracted files in the current directory."
+
+# run gromacs benchmark
+gmx mdrun -s ${SAMPLE}.tpr -nsteps 10000
