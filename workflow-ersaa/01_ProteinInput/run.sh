dccf1a80d76aca8a3c41fc0f7686a72af3dd82eb
esraasuliman <israamohammedrida@gmail.com>
2025-11-23 21:24:03 +0200

    Update run.sh

--- workflow-ersaa/01_ProteinInput/run.sh
+++ workflow-ersaa/01_ProteinInput/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-# Inputs from Silva
-PDB_ID="${PDB_ID}"
-OUTPUT_DIR="/workspace/out"
+PDB_ID="{{inputs.protein_id}}"
 
+OUTPUT_DIR="/workspace/out"
 mkdir -p "$OUTPUT_DIR"
 
 python3 /workspace/download_receptor.py "$PDB_ID" "$OUTPUT_DIR"
