db3aa22b52b545fa3add796a5080f6ab083278e3
esraasuliman <israamohammedrida@gmail.com>
2025-11-25 17:02:21 +0200

    Simplify run.sh by removing unnecessary variables
    Removed variable assignments and output directory creation.

--- workflow-ersaa/07_Docking/run.sh
+++ workflow-ersaa/07_Docking/run.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 set -e
 
-RECEPTOR="{{inputs.receptor_pdbqt}}"
-LIGANDS="{{inputs.ligands_prepared}}"
-GRIDS="{{inputs.grid_boxes}}"
-BOX_SIZE="${BOX_SIZE:-80}"
-
-OUTPUT_DIR="/workspace/out"
-mkdir -p "$OUTPUT_DIR"
-
-python3 /workspace/run_vina.py \
-    "$RECEPTOR" \
-    "$LIGAND" \
-    "$POCKET" \
-    "$BOX_SIZE" \
-    "$OUTPUT_DIR"
+python3 run_vina.py
