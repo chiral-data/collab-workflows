e244f13c84b71326fbca1221c71d3fc47eb66491
esraasuliman <israamohammedrida@gmail.com>
2025-11-25 17:07:53 +0200

    Simplify run.sh by removing unused variables
    Removed variable assignments and output directory creation.

--- workflow-ersaa/08_Reporting/run.sh
+++ workflow-ersaa/08_Reporting/run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 set -e
 
-RESULTS="{{inputs.vina_results}}"
-OUTDIR="/workspace/out"
-
-mkdir -p "$OUTDIR"
-
-python3 /workspace/rank_vina.py "$RESULTS"
-
-# Move output to Silva output directory
-mv binding_affinities.xlsx "$OUTDIR/results.xlsx"
+python3 rank_vina.py
