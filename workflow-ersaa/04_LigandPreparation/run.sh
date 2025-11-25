83f031eab2e418868890fe33c58d0d8eed88e43b
esraasuliman <israamohammedrida@gmail.com>
2025-11-25 16:48:18 +0200

    Simplify run.sh by removing directory variables
    Removed input and output directory variables and adjusted the script to directly call the prepare_ligands.py script.

--- workflow-ersaa/04_LigandPreparation/run.sh
+++ workflow-ersaa/04_LigandPreparation/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-INPUT_DIR="{{inputs.ligands_folder}}"
-OUTPUT_DIR="/workspace/out/ligands_prepared"
-
-mkdir -p "$OUTPUT_DIR"
-
-python3 /workspace/prepare_ligands.py "$INPUT_DIR" "$OUTPUT_DIR"
+python3 prepare_ligands.py
