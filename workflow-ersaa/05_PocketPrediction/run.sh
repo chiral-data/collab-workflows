3596d41133947337eeab4889f769980daffaaaf8
esraasuliman <israamohammedrida@gmail.com>
2025-11-25 16:53:28 +0200

    Simplify run.sh by removing unnecessary variables
    Removed input and output variable definitions and directory creation.

--- workflow-ersaa/05_PocketPrediction/run.sh
+++ workflow-ersaa/05_PocketPrediction/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-INPUT_PDB="{{inputs.receptor_pdb}}"
-OUTPUT_JSON="/workspace/out/pockets.json"
-
-mkdir -p /workspace/out
-
-python3 /workspace/run_p2rank.py "$INPUT_PDB" "$OUTPUT_JSON"
+python3 run_p2rank.py
