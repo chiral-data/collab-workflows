44dedc9749e2e2edb9ae9c2344c57195eaa6cfe1
esraasuliman <israamohammedrida@gmail.com>
2025-11-25 12:52:50 +0200

    Simplify run script by removing variable assignments

--- workflow-ersaa/03_ProteinPreparation/run.sh
+++ workflow-ersaa/03_ProteinPreparation/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-INPUT_PDB="{{inputs.receptor_pdb}}"
-OUTPUT_PDBQT="/workspace/out/receptor.pdbqt"
-
-mkdir -p /workspace/out
-
-python3 /workspace/prepare_receptor.py "$INPUT_PDB" "$OUTPUT_PDBQT"
+python3 prepare_receptor.py
