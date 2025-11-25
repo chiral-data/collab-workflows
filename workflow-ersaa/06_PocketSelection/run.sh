ccceeb365060e9a57017a51a4cc8593d46c50283
esraasuliman <israamohammedrida@gmail.com>
2025-11-25 16:59:07 +0200

    Simplify run.sh by removing variables and mkdir

--- workflow-ersaa/06_PocketSelection/run.sh
+++ workflow-ersaa/06_PocketSelection/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-POCKETS="{{inputs.pockets_json}}"
-OUTPUT_JSON="/workspace/out/grids.json"
-
-mkdir -p /workspace/out
-
-python3 /workspace/generate_grids.py "$POCKETS" "$OUTPUT_JSON"
+python3 generate_grids.py
