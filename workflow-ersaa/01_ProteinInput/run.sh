3924a12197eefa18d50528283f0a039d8ac53c4d
esraasuliman <israamohammedrida@gmail.com>
2025-11-25 10:46:49 +0200

    Simplify run.sh by removing unused variables
    Removed unnecessary variable assignments and directory creation.

--- workflow-ersaa/01_ProteinInput/run.sh
+++ workflow-ersaa/01_ProteinInput/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-PDB_ID="{{inputs.protein_id}}"
-
-OUTPUT_DIR="/workspace/out"
-mkdir -p "$OUTPUT_DIR"
-
-python3 /workspace/download_receptor.py "$PDB_ID" "$OUTPUT_DIR"
+python3 download_receptor.py
